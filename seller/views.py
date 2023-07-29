b841e784f225ebb6380a7a7407c87119c8ae76d5
mostafa7171 <mostafazolfaghari71@gmail.com>
2023-07-29 10:10:18 +0430

    update

--- seller/views.py
+++ seller/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import viewsets, status
 from seller.models import Seller, CreditIncreaseTransaction, CreditDecreaseTransaction, ChargeSaleTransaction
 from seller.serializers import SellerSerializer, CreditIncreaseTransactionSerializer, \
@@ -18,6 +19,37 @@ class CreditIncreaseTransactionViewSet(viewsets.ModelViewSet):
     serializer_class = CreditIncreaseTransactionSerializer
     permission_classes = [AllowAny]
 
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = CreditIncreaseTransactionSerializer(data=request.data)
+            if serializer.is_valid():
+                with transaction.atomic():
+                    serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except ValueError:
+            return Response({"result": "اعتبار ناکافی برای کاهش اعتبار"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+# class CreditDecreaseTransactionViewSet(viewsets.ModelViewSet):
+#     queryset = CreditDecreaseTransaction.objects.all()
+#     serializer_class = CreditDecreaseTransactionSerializer
+#     permission_classes = [AllowAny]
+#
+#     def create(self, request, *args, **kwargs):
+#         try:
+#             serializer = CreditDecreaseTransactionSerializer(data=request.data)
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#         except ValueError:
+#             return Response({"result": "اعتبار ناکافی برای کاهش اعتبار"},
+#                             status=status.HTTP_400_BAD_REQUEST)
+
 
 class CreditDecreaseTransactionViewSet(viewsets.ModelViewSet):
     queryset = CreditDecreaseTransaction.objects.all()
@@ -28,13 +60,32 @@ class CreditDecreaseTransactionViewSet(viewsets.ModelViewSet):
         try:
             serializer = CreditDecreaseTransactionSerializer(data=request.data)
             if serializer.is_valid():
-                serializer.save()
+                with transaction.atomic():
+                    serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         except ValueError:
             return Response({"result": "اعتبار ناکافی برای کاهش اعتبار"},
                             status=status.HTTP_400_BAD_REQUEST)
+
+
+# class ChargeSaleTransactionViewSet(viewsets.ModelViewSet):
+#     queryset = ChargeSaleTransaction.objects.all()
+#     serializer_class = ChargeSaleTransactionSerializer
+#     permission_classes = [AllowAny]
+#
+#     def create(self, request, *args, **kwargs):
+#         try:
+#             serializer = CreditDecreaseTransactionSerializer(data=request.data)
+#             if serializer.is_valid():
+#                 serializer.save()
+#                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#         except ValueError:
+#             return Response({"result": "اعتبار ناکافی برای فروش شارژ."},
+#                             status=status.HTTP_400_BAD_REQUEST)
 
 
 class ChargeSaleTransactionViewSet(viewsets.ModelViewSet):
@@ -44,9 +95,10 @@ class ChargeSaleTransactionViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         try:
-            serializer = CreditDecreaseTransactionSerializer(data=request.data)
+            serializer = ChargeSaleTransactionSerializer(data=request.data)
             if serializer.is_valid():
-                serializer.save()
+                with transaction.atomic():
+                    serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
