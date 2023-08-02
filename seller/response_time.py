15a38274c39c6423882a22b944167e8b745ae794
mostafa7171 <mostafazolfaghari71@gmail.com>
2023-08-02 13:04:39 +0430

    update

--- seller/response_time.py
+++ seller/response_time.py
@@ -27,7 +27,7 @@ def test_low_traffic():
 def test_high_traffic():
     url = "http://127.0.0.1:8000/charge-sale-transactions/"  # Replace with the actual API endpoint URL
     data = {'seller': 1, 'mobile': '09123456789', 'amount': 100}
-    num_requests = 100  # Change this number to adjust the number of requests for high traffic test
+    num_requests = 100
 
     def make_requests():
         for i in range(num_requests):
@@ -38,19 +38,16 @@ def test_high_traffic():
             response_time = end_time - start_time
             print(f"High Traffic - Request {i + 1}: Response Time: {response_time:.5f} seconds")
 
-    # Create multiple threads to simulate high traffic
     threads = []
     for _ in range(5):  # Use 5 threads, each making 20 requests
         thread = threading.Thread(target=make_requests)
         thread.start()
         threads.append(thread)
 
-    # Wait for all threads to finish
     for thread in threads:
         thread.join()
 
 
 if __name__ == "__main__":
-    # Run the low traffic and high traffic test functions
     test_low_traffic()
     test_high_traffic()
